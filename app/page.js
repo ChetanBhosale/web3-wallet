f8504f6c124d62ca4ad85114002ad47b76cab304
Chetan <chetanbhosale810@gmail.com>
2024-11-08 23:10:11 +0530

    added changes

--- app/page.js
+++ app/page.js
@@ -1,101 +1,179 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React, { useState, useEffect } from 'react'
+import { generateMnemonic, mnemonicToSeed } from "bip39";
+import { motion } from "framer-motion"
+import { derivePath } from "ed25519-hd-key";
+import { SiBlockchaindotcom } from "react-icons/si";
+import { PiEye, PiEyeSlash } from "react-icons/pi";
+import { FaRegCopy, FaEthereum } from "react-icons/fa";
+import { MdColorLens } from "react-icons/md"; // Unique icon for theme change
+import { Keypair } from '@solana/web3.js'
+import nacl from "tweetnacl"
+import { Wallet, HDNodeWallet } from "ethers";
+import { ToastContainer, toast } from 'react-toastify'; // Import ToastContainer and toast
+import 'react-toastify/dist/ReactToastify.css'; // Import toast styles
+
+const themes = [
+  'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter'
+];
+
+const page = () => {
+  const [mnemonic, setMnemonic] = useState("dawn shallow blast guide true cake glow lesson refuse cost knee excuse");
+  const [solIndex, setSolIndex] = useState(0);
+  const [ethIndex, setEthIndex] = useState(0);
+  const [wallets, setWallets] = useState([]);
+  const [view, setView] = useState(true);
+  const [showPrivateKeys, setShowPrivateKeys] = useState({});
+  const [currentTheme, setCurrentTheme] = useState('cyberpunk'); // Set initial theme to cyberpunk
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  }, [currentTheme]);
+
+  async function handleGenerateSeedPhrase() {
+    const getMm = await generateMnemonic();
+    setMnemonic(getMm);
+  }
+
+  function copyWord() {
+    navigator.clipboard.writeText(mnemonic);
+    toast.success('Seed phrase copied to clipboard!', {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function handleGenerateSolanaWallet() {
+    const seed = mnemonicToSeed(mnemonic);
+    const path = `m/44'/501'/${solIndex}'/0'`;
+    const derivedSeed = derivePath(path, seed.toString("hex")).key;
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
+    const keypair = Keypair.fromSecretKey(secret);
+    setSolIndex(solIndex + 1);
+    let keys = {
+      publicKey: keypair.publicKey.toBase58(),
+      secretKey: Keypair.fromSecretKey(secret).publicKey.toBase58(),
+      chain: 'Solana'
+    };
+    setWallets([...wallets, keys]);
+  }
+
+  async function handleGenerateEthereumWallet() {
+    const seed = await mnemonicToSeed(mnemonic);
+    const derivationPath = `m/44'/60'/${ethIndex}'/0'`;
+    const hdNode = HDNodeWallet.fromSeed(seed);
+    const child = hdNode.derivePath(derivationPath);
+    const privateKey = child.privateKey;
+    const ethWallet = new Wallet(privateKey);
+    setEthIndex(ethIndex + 1);
+    let keys = {
+      publicKey: ethWallet.address,
+      secretKey: ethWallet.privateKey,
+      chain: 'Ethereum'
+    };
+    setWallets([...wallets, keys]);
+  }
+
+  const togglePrivateKeyVisibility = (index) => {
+    setShowPrivateKeys(prevState => ({
+      ...prevState,
+      [index]: !prevState[index]
+    }));
+  };
+
+  const changeTheme = () => {
+    const nextThemeIndex = (themes.indexOf(currentTheme) + 1) % themes.length;
+    const nextTheme = themes[nextThemeIndex];
+    setCurrentTheme(nextTheme);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='max-w-[1200px] w-full mx-auto p-4'>
+      <ToastContainer />
+      <motion.nav initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} className='w-full my-4 mt-8 flex justify-between items-center'>
+        <div className='flex items-center gap-4 font-bold font-mono text-3xl'>
+          <SiBlockchaindotcom />
+          <p>Peace Wallet</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <motion.button whileHover={{ scale: 1.1, rotate: 10 }} className='btn btn-outline flex items-center gap-2' onClick={changeTheme}>
+            <MdColorLens /> Change Theme
+          </motion.button>
+        </div>
+      </motion.nav>
+      <motion.div initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} className='w-full mt-8 text-center'>
+        <p className='text-5xl font-extrabold mb-4'>Your Own <span className='text-gradient'>Web3</span> Wallet</p>
+        <p className='text-3xl font-bold mb-8'>Support Both <span className='text-primary'>Solana</span> and <span className='text-secondary'>Ethereum</span></p>
+        <motion.button whileHover={{ scale: 1.1 }} className='btn btn-outline mt-8' onClick={handleGenerateSeedPhrase}>
+          <FaRegCopy /> Generate Phrase
+        </motion.button>
+      </motion.div>
+
+      {mnemonic.length > 0 && (
+        <motion.div initial={{ opacity: 0, y: 80 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5 }} className='w-full pb-10 justify-around mt-5 mx-auto max-w-[800px] text-center px-12 bg-white bg-opacity-50 border-2 border-blue-500 gap-6 space-y-6 py-5 rounded-xl shadow-lg'>
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, delay: 1 }} className='w-full justify-end flex gap-4 items-center'>
+            <div className='text-2xl cursor-pointer hover:text-primary' onClick={() => setView(!view)}>
+              {view ? <PiEye /> : <PiEyeSlash />}
+            </div>
+            <div className='text-xl cursor-pointer hover:text-primary' onClick={copyWord}>
+              <FaRegCopy />
+            </div>
+          </motion.div>
+          <div className='grid grid-cols-4 gap-6'>
+            {mnemonic.split(' ').map((ele, index) => (
+              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 1 }} key={index} className={`bg-white text-black py-2 px-6 rounded-full hover:text-primary cursor-pointer ${view && 'blur-sm'}`}>
+                {ele}
+              </motion.div>
+            ))}
+          </div>
+        </motion.div>
+      )}
+
+      {mnemonic.length > 0 && (
+        <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: 2 }} className="flex gap-4 justify-center mt-8">
+          <motion.button whileHover={{ scale: 1.1 }} className='btn btn-primary mt-8 flex items-center gap-2' onClick={handleGenerateSolanaWallet}>
+            <SiBlockchaindotcom /> Generate Solana key pair
+          </motion.button>
+          <motion.button whileHover={{ scale: 1.1 }} className='btn btn-secondary mt-8 flex items-center gap-2' onClick={handleGenerateEthereumWallet}>
+            <FaEthereum /> Generate Ethereum key pair
+          </motion.button>
+        </motion.div>
+      )}
+
+      <div className='mt-8 space-y-4'>
+        {wallets.map((w, index) => (
+          <motion.div 
+            key={index} 
+            initial={{ opacity: 0, y: 20 }} 
+            animate={{ opacity: 1, y: 0 }} 
+            transition={{ duration: 0.5 }} 
+            className='bg-gray-800 shadow-lg rounded-lg p-6 w-full transform hover:scale-105 transition-transform duration-300'
+          >
+            <span className={`badge ${w.chain === 'Solana' ? 'bg-primary' : 'bg-secondary'}`}>{w.chain}</span>
+            <div className='mt-4'>
+              <label className='block font-bold text-white'>Public Key:</label>
+              <input type='text' value={w.publicKey} readOnly className='w-full p-2 border rounded bg-white text-black' />
+            </div>
+            <div className='mt-4'>
+              <label className='block font-bold text-white'>Secret Key:</label>
+              <div className='flex items-center'>
+                <input type={showPrivateKeys[index] ? 'text' : 'password'} value={w.secretKey} readOnly className='w-full p-2 border rounded bg-white text-black' />
+                <button onClick={() => togglePrivateKeyVisibility(index)} className='ml-2 text-white'>
+                  {showPrivateKeys[index] ? <PiEyeSlash /> : <PiEye />}
+                </button>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
     </div>
   );
 }
+
+export default page;
